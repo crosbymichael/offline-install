20eddbfe5d4d894cfee6974829c7d3981acba1be
Michael Crosby <crosbymichael@gmail.com>
2018-08-07 14:17:27 -0400

    Fix arg count

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 }
 
 func run(ctx context.Context, path string) error {
-	if len(os.Args) != 2 {
+	if len(os.Args) != 3 {
 		return fmt.Errorf("expect 2 arguments <tar> <name>")
 	}
 	client, err := containerd.New(defaults.DefaultAddress)
